04a599b2eb5e87c84fe6a4f26f5357a8cab889df
sw4 <sw4@users.noreply.github.com>
2014-11-27 15:40:30 +0000

    Update colorpicker.js

--- src/colorpicker/colorpicker.js
+++ src/colorpicker/colorpicker.js
@@ -34,75 +34,21 @@ Javascript:
         var type = (window.SVGAngle || document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1") ? "SVG" : "VML"),
             listeners = opt.listeners === undefined ? {} : opt.listeners,
             el = opt.element,
-            value = opt.value || '#000000',
-            tpl = "<div class='ui-colorpicker'>\
-				<div class='ui-colorpicker-area'>\
-					<div class='ui-colorpicker-slPicker'></div>\
-				</div>\
-				<div class='ui-colorpicker-range'>\
-					<div class='ui-colorpicker-hPicker'></div>\
-				</div>\
-			</div>";
+            value = opt.value || '#000000';
 
-        el = $ui.replaceEl(el, tpl);
+        el = $ui.replaceEl(el, $ui.compile('colorpicker'));
         var areaTpl, areaEl, rangeTpl, rangeEl;
-        if (type === 'SVG') {
-            areaTpl = "<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='100%' height='100%'>\
-				<defs>\
-					<linearGradient id='gradient-black-0' x1='0%' y1='100%' x2='0%' y2='0%'>\
-						<stop offset='0%' stop-color='#000000' stop-opacity='1'></stop>\
-						<stop offset='100%' stop-color='#CC9A81' stop-opacity='0'></stop>\
-					</linearGradient>\
-					<linearGradient id='gradient-white-0' x1='0%' y1='100%' x2='100%' y2='100%'>\
-						<stop offset='0%' stop-color='#FFFFFF' stop-opacity='1'></stop>\
-						<stop offset='100%' stop-color='#CC9A81' stop-opacity='0'></stop>\
-					</linearGradient>\
-				</defs>\
-				<rect x='0' y='0' width='100%' height='100%' fill='url(#gradient-white-0)'></rect>\
-				<rect x='0' y='0' width='100%' height='100%' fill='url(#gradient-black-0)'></rect>\
-			</svg>";
-            rangeTpl = "<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='100%' height='100%'>\
-				<defs>\
-					<linearGradient id='gradient-hsv-0' x1='0%' y1='100%' x2='0%' y2='0%'>\
-						<stop offset='0%' stop-color='#FF0000' stop-opacity='1'></stop>\
-						<stop offset='13%' stop-color='#FF00FF' stop-opacity='1'></stop>\
-						<stop offset='25%' stop-color='#8000FF' stop-opacity='1'></stop>\
-						<stop offset='38%' stop-color='#0040FF' stop-opacity='1'></stop>\
-						<stop offset='50%' stop-color='#00FFFF' stop-opacity='1'></stop>\
-						<stop offset='63%' stop-color='#00FF40' stop-opacity='1'></stop>\
-						<stop offset='75%' stop-color='#0BED00' stop-opacity='1'></stop>\
-						<stop offset='88%' stop-color='#FFFF00' stop-opacity='1'></stop>\
-						<stop offset='100%' stop-color='#FF0000' stop-opacity='1'></stop>\
-					</linearGradient>\
-				</defs>\
-				<rect x='0' y='0' width='100%' height='100%' fill='url(#gradient-hsv-0)'></rect>\
-			</svg>";
-        } else if (type === 'VML') {
-
-            areaTpl = "<DIV style='position: relative; width: 100%; height: 100%'>\
-				<v:rect style='position: absolute; top: 0; left: 0; width: 100%; height: 100%' stroked='f' filled='t'>\
-				<v:fill type='gradient' method='none' angle='0' color='red' color2='red' colors='8519f fuchsia;.25 #8000ff;24903f #0040ff;.5 aqua;41287f #00ff40;.75 #0bed00;57671f yellow'></v:fill>\
-				</v:rect>\
-			</DIV>";
-
-            rangeTpl = "<DIV style='position: relative; width: 100%; height: 100%'>\
-				<v:rect style='position: absolute; left: -1px; top: -1px; width: 101%; height: 101%' stroked='f' filled='t'>\
-					<v:fill type='gradient' method='none' angle='270' color='#FFFFFF' opacity='100%' color2='#CC9A81' o:opacity2='0%'></v:fill>\
-				</v:rect>\
-				<v:rect style='position: absolute; left: 0px; top: 0px; width: 100%; height: 101%' stroked='f' filled='t'>\
-					<v:fill type='gradient' method='none' angle='0' color='#000000' opacity='100%' color2='#CC9A81' o:opacity2='0%'></v:fill>\
-				</v:rect>\
-			</DIV>";
-
+        if (type === 'SVG') {            
+			areaEl = $ui.createEl($ui.compile('colorpicker_svg_sl'));
+			rangeEl = $ui.createEl($ui.compile('colorpicker_svg_h'));
+        } else if (type === 'VML') {            
+			areaEl = $ui.createEl($ui.compile('colorpicker_vml_sl'));
+			rangeEl = $ui.createEl($ui.compile('colorpicker_vml_h'));
             if (!document.namespaces.v) {
                 document.namespaces.add('v', 'urn:schemas-microsoft-com:vml', '#default#VML');
             }
         }
 
-
-
-        areaEl = $ui.createEl(areaTpl);
-        rangeEl = $ui.createEl(rangeTpl);
         el.children[0].appendChild(areaEl);
         el.children[1].appendChild(rangeEl);
 
